631c0d055f16c9dc95e7a97771e25c13222ea5f7
adoc <adoc@code.webmob.net>
2014-12-13 20:27:22 -0800

    update models to require `init_models` and passing of settings.

--- cap/models.py
+++ cap/models.py
@@ -15,56 +15,62 @@ DBSession = sqlalchemy.orm.scoped_session(
 Base = sqlalchemy.ext.declarative.declarative_base()
 
 
-class EpochDays(sqlalchemy.types.TypeDecorator):
+def init_models(settings):
+    """To be run during application initialization. This allows for the passing
+    of config options.
     """
-    http://docs.sqlalchemy.org/en/rel_0_9/core/types.html#sqlalchemy.types.TypeDecorator
-    """
-    impl = sqlalchemy.types.Integer
-    epoch = datetime.datetime(1970, 1, 1).replace(tzinfo=pytz.utc)
+    global Location
+    global LocationDayQuantity
 
-    def process_bind_param(self, value, dialect):
-        return (value - self.epoch).days
+    date_format = settings['date_format']
 
-    def process_result_value(self, value, dialect):
-        return self.epoch + datetime.timedelta(days=value)
+    class EpochDays(sqlalchemy.types.TypeDecorator):
+        """
+        http://docs.sqlalchemy.org/en/rel_0_9/core/types.html#sqlalchemy.types.TypeDecorator
+        """
+        impl = sqlalchemy.types.Integer
+        epoch = datetime.datetime(1970, 1, 1).replace(tzinfo=pytz.timezone(settings['local_timezone']))
 
+        def process_bind_param(self, value, dialect):
+            return (value - self.epoch).days
 
-class Location(Base):
-    __tablename__ = 'locations'
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    display_name = sqlalchemy.Column(sqlalchemy.String(64), index=True,
-                                        unique=True)
-    capacity = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+        def process_result_value(self, value, dialect):
+            return self.epoch + datetime.timedelta(days=value)
 
-    dyn_day_quantities = sqlalchemy.orm.relationship("LocationDayQuantity",
-                                                    lazy="dynamic")
+    class Location(Base):
+        __tablename__ = 'locations'
+        id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+        display_name = sqlalchemy.Column(sqlalchemy.String(64), index=True,
+                                            unique=True)
+        capacity = sqlalchemy.Column(sqlalchemy.Integer, default=0)
 
-    def __json__(self, request):
-        local_tz = pytz.timezone(request.registry.settings['local_timezone'])
-        return {
-            'id': self.id,
-            'display_name': self.display_name,
-            'capacity': self.capacity,
-            'day_quantities': (
-                self.dyn_day_quantities.filter(
-                                LocationDayQuantity.date >=
-                                    datetime.datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(local_tz).replace(tzinfo=pytz.utc)) # This is a bullshit hack.
-                                .all())
-        }
+        dyn_day_quantities = sqlalchemy.orm.relationship("LocationDayQuantity",
+                                                        lazy="dynamic")
 
+        def __json__(self, request):
+            return {
+                'id': self.id,
+                'display_name': self.display_name,
+                'capacity': self.capacity,
+                'day_quantities': (
+                    self.dyn_day_quantities.filter(
+                                    LocationDayQuantity.date >=
+                                        cap.util.get_localized_datetime(request))
+                                    .all())
+            }
 
-class LocationDayQuantity(Base):
-    __tablename__ = 'location_amounts'
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    date = sqlalchemy.Column(EpochDays, nullable=False)
-    location_id = sqlalchemy.Column(sqlalchemy.Integer,
-                    sqlalchemy.ForeignKey(Location.id), nullable=False)
-    location = sqlalchemy.orm.relationship(Location, backref="day_quantities")
-    amount = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    class LocationDayQuantity(Base):
+        __tablename__ = 'location_amounts'
+        id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+        date = sqlalchemy.Column(EpochDays, nullable=False)
+        location_id = sqlalchemy.Column(sqlalchemy.Integer,
+                        sqlalchemy.ForeignKey(Location.id), nullable=False)
+        location = sqlalchemy.orm.relationship(Location, backref="day_quantities")
+        amount = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
 
-    def __json__(self, request):
-        date_format = request.registry.settings['date_format']
-        return {
-            'date': self.date.strftime(date_format),
-            'amount': self.amount
-        }
+        def __json__(self, request):
+            
+            return {
+                'date': self.date.strftime(date_format),
+                'amount': self.amount
+            }
